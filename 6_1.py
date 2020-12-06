029b4daaf76ea18dc416bc051cabb521f48a8964
szeko34 <42772637+szeko34@users.noreply.github.com>
2020-12-06 19:56:13 +0100

    Update and rename das.py to 6_1.py

--- 6_1.py
+++ 6_1.py
@@ -1,9 +1,8 @@
-subor=open("6_1input.txt","r")# Tu napíšte svoj kód :-)
+subor=open("6_1input.txt","r")
 
 s=subor.read().split("\n\n")
 
 abc="qwertzuioplkjhgfdsayxcvbnm"
-print(len(abc))
 
 suc=0
 
@@ -11,9 +10,7 @@ for r in s:
     a=r.split("\n")
     print(a)
     for i in abc:
-        #print(i,[i in j for j in a])
         if all([i in j for j in a]):
-            #print(i,[i in j for j in a])
-            suc+=1
+           suc+=1
 
 print(suc)

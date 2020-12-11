b0045d998de795bcd0a20f02d41bc649a00b33a0
szeko34 <42772637+szeko34@users.noreply.github.com>
2020-12-11 20:09:07 +0100

    day 11

---
+++ d11/11_2.py
@@ -0,0 +1,66 @@
+subor=open("11_1input.txt","r")
+
+pole = []
+
+riadky=[x.strip() for x in subor.readlines()]
+
+pole.append(list("."*(len(riadky[0])+2)))
+
+for x in riadky:
+    pole.append(list("."+x+"."))
+
+pole.append(list("."*(len(riadky[0])+2)))
+
+#print(pole)
+
+def susedov(p,x,y):
+    tmp = 0
+    for i in -1,0,1:
+        for j in -1,0,1:
+            c=1
+            while 0<=x+c*i<len(p) and 0<=y+c*j<len(p[0]) and p[x+c*i][y+c*j] == ".":
+               c+=1
+            if 0<=x+c*i<len(p) and 0<=y+c*j<len(p[0]):
+                tmp += p[x+c*i][y+c*j] == "#"
+    if p[x][y] == "#":
+        return tmp - 1
+    else:
+        return tmp
+
+def vypis(pole):
+    for x in pole:
+        print(x)
+
+def krok(p):
+    nove=[list("."*(len(riadky[0])+2))]
+    for i in range(1,len(p)-1):
+        r=["."]
+        for j in range(1,len(p[0])-1):
+            #print(susedov(p,i,j))
+            if p[i][j] == "L" and susedov(p,i,j) == 0:
+                r.append("#")
+            elif p[i][j] == "#" and susedov(p,i,j) >= 5:
+                r.append("L")
+            else:
+                r.append(p[i][j])
+        r.append(".")
+        nove.append(r)
+    nove.append(list("."*(len(riadky[0])+2)))
+    #vypis(nove)
+    return(nove)
+
+nove=pole
+stare = []
+k=0
+while nove != stare:
+    stare=nove
+    nove=krok(stare)
+    k += 1
+    #print(k)
+
+sedadiel = 0
+for x in nove:
+    sedadiel+= x.count("#")
+
+print(sedadiel)
+

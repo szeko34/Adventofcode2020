a95d537a56da7454302324523f49e0798eb22eec
szeko34 <42772637+szeko34@users.noreply.github.com>
2020-12-15 08:46:21 +0100

    d15

--- d15/15_1.py
+++ d15/15_1.py
@@ -1,13 +1,13 @@
-cisla=[int(x) for x in "0,5,4,1,10,14,7".split(",")]
+cisla = [int(x) for x in "0,5,4,1,10,14,7".split(",")]
 
-i=6
+i = 6
 
 
-while i<(2020-1):
+while i < (2020 - 1):
     if cisla.count(cisla[i]) == 1:
         cisla.append(0)
     else:
-        cisla.append(i-(len(cisla) - cisla[-2::-1].index(cisla[i])-2))
+        cisla.append(i - (len(cisla) - cisla[-2::-1].index(cisla[i]) - 2))
     i += 1
 
-print(len(cisla),cisla[-1])
+print(len(cisla), cisla[-1])
--- d15/15_2.py
+++ d15/15_2.py
@@ -1,18 +1,16 @@
-cisla=[int(x) for x in "0,5,4,1,10,14,7".split(",")]
+cisla = [int(x) for x in "0,5,4,1,10,14,7".split(",")]
 
-i=7
+i = 7
 
-c={}
+c = {}
 for j in range(len(cisla)):
-    c[cisla[j]]=j
-
+    c[cisla[j]] = j
 dalsi = 0
 
-while i<(30000000-1):
+while i < (30000000 - 1):
     if dalsi not in c.keys():
-        c[dalsi],dalsi=i,0
+        c[dalsi], dalsi = i, 0
     else:
-        c[dalsi],dalsi = i,i-c[dalsi]
-    i+=1
-
+        c[dalsi], dalsi = i, i - c[dalsi]
+    i += 1
 print(dalsi)
